522391ffc8aea19c8fc42e3c10d971f40f3aec63
Sanan Bozkurt <hola@sananbozkurt.ru>
2017-04-08 18:52:29 +0300

    Добавлен контент

---
+++ content/inc/mysql.php
@@ -0,0 +1,7 @@
+<?
+defined('_SANANBOZ') or die('Forbidden');
+$IP= '';
+$mysqlcon=@mysql_pconnect('localhost','u793869259','AeZaKmI91') or die('{"error":"MySQL database is not available."}');
+$mysqlseldb=mysql_select_db('u793869259');
+mysql_query("SET NAMES UTF8",$mysqlcon);
+?>
